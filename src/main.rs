d4d9b97aacba8decadc7285ef34fb86bd8bbb508
Damon Smith <damon@larrymite.com.au>
2019-07-29 20:21:47 +1000

    Multithreaded (#2)
    * move resize and frame generation into threads
    
    * use chunks and a container struct to allow storing frames inside the thread
    
    * remove unnecessary
    
    * pre-thread clean version
    
    * even cleaner pre-thread version
    
    * pre-thread with frame_numbers vec
    
    * multithreaded complete

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,91 @@
-extern crate raster;
 extern crate clap;
 extern crate gif;
-use clap::{Arg, App};
-use raster::{transform, editor, BlendMode, PositionMode};
-use gif::{Frame, Encoder, Repeat, SetParameter};
+extern crate raster;
+use gif::{Encoder, Frame, Repeat, SetParameter};
+use raster::{editor, transform, BlendMode, PositionMode};
+use std::env;
 use std::fs::File;
+use std::thread;
+
+const NUMTHREADS: usize = 4;
+const FRAME_COUNT: usize = 12;
+
 fn main() {
+	let args: Vec<String> = env::args().collect();
+	let _image_file_name = args[1].clone();
+	let _src_image = raster::open(&_image_file_name).unwrap();
 
-    let _matches = App::new("Image to expander animation generator")
-        .version("1.0")
-        .author("Damon Smith <damon.default@gmail.com>")
-        .about("Spins and expands an image to create an animated gif")
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input file to use")
-            .required(true)
-            .index(1))
-        .get_matches();
-    
-    let frame_count = 12;
-    let image_file_name = _matches.value_of("INPUT").unwrap();
-    let mut _src_image = raster::open(image_file_name).unwrap();
+	let mut gif_output = File::create("output.gif").unwrap();
+	let mut encoder = Encoder::new(
+		&mut gif_output,
+		_src_image.width as u16,
+		_src_image.height as u16,
+		&[],
+	)
+	.unwrap();
+	encoder.set(Repeat::Infinite).unwrap();
 
-    let resize_pixels_per_frame = _src_image.height / frame_count;
+	let mut frame_numbers: Vec<i32> = (0..FRAME_COUNT as i32).collect();
+	let chunks = frame_numbers.chunks_mut(FRAME_COUNT / NUMTHREADS);
 
-    println!("will resize {} pixels per frame", resize_pixels_per_frame);
+	let mut children = vec![];
 
-    let mut gif_output = File::create("output.gif").unwrap();
-    let mut encoder = Encoder::new(&mut gif_output, _src_image.width as u16, _src_image.height as u16, &[]).unwrap();
-    encoder.set(Repeat::Infinite).unwrap();
+	// Divide the set of frames up into a chunk of frames for each thread
+	println!("frames: {}, threads: {}", FRAME_COUNT, NUMTHREADS);
+	for chunk in chunks {
+		let nums_chunk = chunk.to_owned();
+		let src = _src_image.clone();
+		children.push(thread::spawn(move || {
+			let frames: Vec<Frame> = nums_chunk
+				.iter()
+				.map(|frame_number| {
+					return generate_gif_frame(&src, frame_number);
+				})
+				.collect();
+			return frames;
+		}));
+	}
 
-    for i in 0..frame_count {
-        
-        let mut _inner_image = _src_image.clone();
-        let mut _center_image = _src_image.clone();
-        let mut _outer_image = _src_image.clone();
-        
-        transform::resize_exact_height(&mut _inner_image, i*resize_pixels_per_frame).unwrap();
-        transform::resize_exact_height(&mut _center_image, _src_image.height + i*resize_pixels_per_frame).unwrap();
-        transform::resize_exact_height(&mut _outer_image, (_src_image.height*2) + i*resize_pixels_per_frame).unwrap();
-        
-        let mut overlay = editor::blend(&_outer_image, &_center_image, BlendMode::Normal, 1.0, PositionMode::Center, 0, 0).unwrap();
-        overlay = editor::blend(&overlay, &_inner_image, BlendMode::Normal, 1.0, PositionMode::Center, 0, 0).unwrap();
-        
-        editor::crop(&mut overlay, _src_image.width, _src_image.height, PositionMode::Center, 0, 0).unwrap();
-        
-        let _frame = Frame::from_rgba(_src_image.width as u16, _src_image.height as u16, overlay.bytes.as_mut_slice());
-        encoder.write_frame(&_frame).unwrap();
-    }
+	for handle in children {
+		let frames = handle.join().unwrap();
+		for frame in frames {
+			encoder.write_frame(&frame).unwrap();
+		}
+	}
+}
+
+fn generate_gif_frame<'a>(src: &raster::Image, frame_number: &i32) -> Frame<'a> {
+	let resize_amount = frame_number * (src.height / FRAME_COUNT as i32);
+	let mut _outer_image = src.clone();
+	let mut _inner_image = src.clone();
+	let mut _center_image = src.clone();
+	let width = src.width;
+	let height = src.height;
+
+	transform::resize_exact_height(&mut _inner_image, resize_amount).unwrap();
+	transform::resize_exact_height(&mut _center_image, height + resize_amount).unwrap();
+	transform::resize_exact_height(&mut _outer_image, (height * 2) + resize_amount).unwrap();
+	let mut overlay = editor::blend(
+		&_outer_image,
+		&_center_image,
+		BlendMode::Normal,
+		1.0,
+		PositionMode::Center,
+		0,
+		0,
+	)
+	.unwrap();
+	overlay = editor::blend(
+		&overlay,
+		&_inner_image,
+		BlendMode::Normal,
+		1.0,
+		PositionMode::Center,
+		0,
+		0,
+	)
+	.unwrap();
+	editor::crop(&mut overlay, width, height, PositionMode::Center, 0, 0).unwrap();
+	println!("generated gif frame: {}", frame_number);
+	return Frame::from_rgba(width as u16, height as u16, overlay.bytes.as_mut_slice());
 }
